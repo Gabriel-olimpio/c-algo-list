dd15d22ba773ed353a63eb47524a8257e5d7c4e9
Gabriel <gabrielandre699@gmail.com>
2024-04-22 10:27:17 -0300

    rep2

---
+++ rep2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Escrever um algoritmo que gera e escreve os números ímpares entre 100 e 200.
+
+int main(){
+    int i, n, impar;
+    for (i = 100; i < 200; i++){
+        if (i % 2 != 0){
+            printf("%d ", i);
+        }
+    }
+}
